7ea7d950e0dbb04432a2e44b6406e6911a6bafd4
SWEATA06 <164531847+SWEATA06@users.noreply.github.com>
2025-04-17 08:25:33 +0530

    use set to store vowels

--- leetcode-1456.cpp
+++ leetcode-1456.cpp
@@ -20,3 +20,18 @@ public:
         }return maxi;
     }
 };
+//better
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+        int n=s.size();
+       unordered_set<char> vowels={'a','e','i','o','u'};
+       int count=0;
+       int maxi=0;
+       for(int i=0;i<n;i++){
+        if(vowels.count(s[i])) count++;
+        if(i>=k && vowels.count(s[i-k])) count--;
+        maxi=max(maxi,count);
+       } return maxi;
+    }
+};
